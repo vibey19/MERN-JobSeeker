4cad41d1e167d8ce6a875301ed7fa833d2482be1
Jainil Dadhaniya <jdadhaniya6@gmail.com>
2024-08-27 21:52:46 +0530

    routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./layout/AppLayout";
+import LandingPage from "./pages/LandingPage";
+import OnBoarding from "./pages/OnBoarding";
+import JobListing from "./pages/JobListing";
+import PostJob from "./pages/PostJob";
+import SavedJobs from "./pages/SavedJobs";
+import MyJobs from "./pages/MyJobs";
+import JobPage from "./pages/Job";
 import "./App.css";
 
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/onboarding",
+        element: <OnBoarding />,
+      },
+      {
+        path: "/jobs",
+        element: <JobListing />,
+      },
+      {
+        path: "/job/:id",
+        element: <JobPage />,
+      },
+      {
+        path: "/post-job",
+        element: <PostJob />,
+      },
+      {
+        path: "/saved-jobs",
+        element: <SavedJobs />,
+      },
+      {
+        path: "/my-jobs",
+        element: <MyJobs />,
+      },
+    ],
+  },
+]);
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ src/pages/OnBoarding.jsx
@@ -0,0 +1,5 @@
+const OnBoarding = () => {
+  return <div></div>;
+};
+
+export default OnBoarding;
