7effc3487d10bd2d94f1a38bcf474c9683eddccf
Jainil Dadhaniya <jdadhaniya6@gmail.com>
2024-08-28 15:05:46 +0530

    completed landingPage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 function Header() {
   return (
@@ -9,6 +15,13 @@ function Header() {
           <img src="/logo.png" alt="workhive logo" className="h-7 w-25" />
         </Link>
         <Button variant="outline">Login</Button>
+
+        {/* <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn> */}
       </nav>
     </div>
   );
